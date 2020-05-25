58440984f120b53c34e38f95d66e85ec10eec7a4
Masso Ferdine <mmsferdine@gmail.com>
2020-05-25 11:22:11 +0100

    add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+ def show 
+ @user = User.find(params[:id])
+ @articles = @user.articles
+ end
+
 def new 
    @user = User.new 
 end
@@ -28,6 +33,8 @@ def create
         render 'new'
     end
 end
+
+
 
 private
 
