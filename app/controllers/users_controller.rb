847aaef7b9e447dbfa332498a1401b3b889f0780
Masso Ferdine <mmsferdine@gmail.com>
2020-06-01 11:04:23 +0100

    Add helper methods to use in views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
+before_action :set_user, only: [:show, :edit, :update]
+
  def show 
- @user = User.find(params[:id])
  @articles = @user.articles.paginate(page: params[:page], per_page: 3)
  end
 
@@ -14,12 +15,10 @@ def new
 end
 
 def edit
-    @user = User.find(params[:id])
 end
 
 def update 
-    @user = User.find(params[:id])
-    if @user.update(user_params)
+        if @user.update(user_params)
         flash[:notice] = "Your account was successfully updated"
         redirect_to @user
     else
@@ -31,6 +30,7 @@ end
 def create 
     @user = User.new(user_params)
     if @user.save 
+        session[:user_id] = @user.id
         flash[:notice] = "Welcome to the Alpha  blog: #{@user.username}" 
         redirect_to articles_path
     else 
@@ -45,5 +45,7 @@ private
 def user_params
 params.require(:user).permit(:username, :email, :password)
 end
-
+def set_user 
+    @user = User.find(params[:id])
+end
 end
